38d21a15940bf565e4c03a564d269ffdb912236f
Nazar_Dutka <56367658+na1xpro@users.noreply.github.com>
2021-08-23 13:18:30 +0300

    Final Version

--- bot_run.py
+++ bot_run.py
@@ -2,39 +2,35 @@ import telebot
 import requests
 from bs4 import BeautifulSoup
 from telebot import types
-import random
 
-TOKEN  = '1996688275:AAHPXZ9CQoUHUbt5eIAxScPirRV6gcpMySw'
+
+TOKEN = '1996688275:AAHPXZ9CQoUHUbt5eIAxScPirRV6gcpMySw'
 
 bot = telebot.TeleBot(TOKEN)
 
 responce_bitcoin = requests.get("https://coinremitter.com/api/v2/get-coin-rate")
 
-
 data = responce_bitcoin.json()
 
-@bot.message_handler(commands = ['start'])
+
+@bot.message_handler(commands=['start'])
 def start(message):
-
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("BTC")
-    item2 = types.KeyboardButton("DOG")
+    item2 = types.KeyboardButton("DOGE")
     item3 = types.KeyboardButton("BCH")
     item4 = types.KeyboardButton("TCN")
     item5 = types.KeyboardButton("DASH")
     item6 = types.KeyboardButton("USDTERC20")
     item7 = types.KeyboardButton("BNB")
 
+    markup.add(item1, item2, item3, item4, item5, item6, item7, )
+
+    bot.send_message(message.chat.id, 'Hello, again!'.format(message.from_user), reply_markup=markup)
 
 
-
-    markup.add(item1,item2,item3,item4,item5,item6,item7,)
-
-    bot.send_message(message.chat.id,'Hello, again!'.format(message.from_user),reply_markup = markup)
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
-
     if message.text == "BTC":
         bot.send_message(message.chat.id, data['data']['BTC']['price']).format(message.from_user)
     elif message.text == "LTC":
@@ -51,9 +47,8 @@ def bot_message(message):
         bot.send_message(message.chat.id, data['data']['USDTERC20']['price']).format(message.from_user)
     elif message.text == "BNB":
         bot.send_message(message.chat.id, data['data']['BNB']['price']).format(message.from_user)
-
-
-
+    else:
+        bot.send_message(message.chat.id, f' - невідома валюта! Введіть корректну!').format(message.from_user)
 
 
 
